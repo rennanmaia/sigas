9b76d064cf2eac70950cd6c5ef9435391094df47
kelw.prod@gmail.com <kelw.prod@gmail.com>
2025-12-29 18:30:33 -0300

    feat: create profile faker data and schema

---
+++ src/features/profiles/data/data.ts
@@ -0,0 +1,24 @@
+import { Shield, UserCheck, Users, CreditCard } from "lucide-react";
+
+export const roles = [
+  {
+    label: "Superadmin",
+    value: "superadmin",
+    icon: Shield,
+  },
+  {
+    label: "Admin",
+    value: "admin",
+    icon: UserCheck,
+  },
+  {
+    label: "Manager",
+    value: "manager",
+    icon: Users,
+  },
+  {
+    label: "Cashier",
+    value: "cashier",
+    icon: CreditCard,
+  },
+] as const;
---
+++ src/features/profiles/data/schema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod'
+
+const profileSchema = z.object({
+  id: z.string(),
+  label: z.string(),
+  value: z.string(),
+  description: z.string().optional(),
+  usersCount: z.number().optional().catch(0),
+})
+export type Profile = z.infer<typeof profileSchema>
+export const profileListSchema = z.array(profileSchema)
