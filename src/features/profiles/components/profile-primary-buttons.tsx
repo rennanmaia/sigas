a9d42dca035850dcb0fa34601820ae0e890ef318
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-02 17:48:46 -0300

    feat: link add profile to the create screen

--- src/features/profiles/components/profile-primary-buttons.tsx
+++ src/features/profiles/components/profile-primary-buttons.tsx
@@ -1,13 +1,14 @@
 import { ShieldPlus } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { useProfiles } from './profiles-provider'
+import { Link } from '@tanstack/react-router'
 
 export function UsersPrimaryButtons() {
-  const { setOpen } = useProfiles()
   return (
     <div className='flex gap-2'>
-      <Button className='space-x-1' onClick={() => setOpen('add')}>
-        <span>Add Profile</span> <ShieldPlus size={18} />
+      <Button asChild className='space-x-1'>
+        <Link to='/profiles/create'>
+          <span>Add Profile</span> <ShieldPlus size={18} />
+        </Link>
       </Button>
     </div>
   )
