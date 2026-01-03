b35e53dc125600a1c871c52eaf57c1d4ba2db746
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-03 20:55:29 -0300

    feat: add new dialogs

--- src/features/users/components/users-dialogs.tsx
+++ src/features/users/components/users-dialogs.tsx
@@ -1,6 +1,8 @@
 import { UsersActionDialog } from './users-action-dialog'
 import { UsersDeleteDialog } from './users-delete-dialog'
 import { UsersInviteDialog } from './users-invite-dialog'
+import { UsersViewDialog } from './users-view-dialog'
+import { UsersToggleStatusDialog } from './users-toggle-status-dialog'
 import { useUsers } from './users-provider'
 
 export function UsersDialogs() {
@@ -38,6 +40,30 @@ export function UsersDialogs() {
             open={open === 'delete'}
             onOpenChange={() => {
               setOpen('delete')
+              setTimeout(() => {
+                setCurrentRow(null)
+              }, 500)
+            }}
+            currentRow={currentRow}
+          />
+
+          <UsersViewDialog
+            key={`user-view-${currentRow.id}`}
+            open={open === 'view'}
+            onOpenChange={() => {
+              setOpen('view')
+              setTimeout(() => {
+                setCurrentRow(null)
+              }, 500)
+            }}
+            currentRow={currentRow}
+          />
+
+          <UsersToggleStatusDialog
+            key={`user-toggle-${currentRow.id}`}
+            open={open === 'toggleStatus'}
+            onOpenChange={() => {
+              setOpen('toggleStatus')
               setTimeout(() => {
                 setCurrentRow(null)
               }, 500)
