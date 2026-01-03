66d6bdd8f25c9475c3967e6f2f70c443cd445f33
João Carvalho <cjoao7610@gmail.com>
2026-01-02 22:43:06 -0300

    feat: add search validation schema for projects route

--- src/routes/_authenticated/projects/index.tsx
+++ src/routes/_authenticated/projects/index.tsx
@@ -1,6 +1,12 @@
 import { Projects } from "@/features/projects";
 import { createFileRoute } from "@tanstack/react-router";
-
+import z from "zod";
+const projectsSearchSchema = z.object({
+  filter: z.string().optional().catch(""),
+  type: z.enum(["all", "concluido", "emAndamento"]).optional().catch("all"),
+  sort: z.enum(["asc", "desc"]).optional().catch("asc"),
+});
 export const Route = createFileRoute("/_authenticated/projects/")({
+  validateSearch: (search) => projectsSearchSchema.parse(search),
   component: Projects,
 });
