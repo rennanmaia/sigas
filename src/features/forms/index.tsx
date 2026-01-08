de53b6ffd4b9541b867c9a0c09d1f624c174946c
João Carvalho <cjoao7610@gmail.com>
2026-01-08 18:29:40 -0300

    feat: provide forms main page with form provider

--- src/features/forms/index.tsx
+++ src/features/forms/index.tsx
@@ -7,6 +7,7 @@ import { ConfigDrawer } from "@/components/config-drawer";
 import { type NavigateFn } from "@/hooks/use-table-url-state";
 import { FormsTable } from "./components/forms-table";
 import { FormsPrimaryButtons } from "./components/forms-primary-button";
+import { FormsProvider } from "./components/forms-provider";
 
 interface Props {
   search: Record<string, unknown>;
@@ -16,27 +17,29 @@ interface Props {
 export default function FormList({ search, navigate }: Props) {
   return (
     <>
-      <Header>
-        <Search />
-        <div className="ms-auto flex items-center gap-4">
-          <ThemeSwitch />
-          <ConfigDrawer />
-          <ProfileDropdown />
-        </div>
-      </Header>
-
-      <Main className="flex flex-1 flex-col gap-4 sm:gap-6">
-        <div className="flex flex-wrap items-end justify-between gap-2">
-          <div>
-            <h2 className="text-2xl font-bold tracking-tight">Formulários</h2>
-            <p className="text-muted-foreground">
-              Gerencie e publique seus formulários
-            </p>
+      <FormsProvider>
+        <Header>
+          <Search />
+          <div className="ms-auto flex items-center gap-4">
+            <ThemeSwitch />
+            <ConfigDrawer />
+            <ProfileDropdown />
           </div>
-          <FormsPrimaryButtons />
-        </div>
-        <FormsTable search={search} navigate={navigate} />
-      </Main>
+        </Header>
+
+        <Main className="flex flex-1 flex-col gap-4 sm:gap-6">
+          <div className="flex flex-wrap items-end justify-between gap-2">
+            <div>
+              <h2 className="text-2xl font-bold tracking-tight">Formulários</h2>
+              <p className="text-muted-foreground">
+                Gerencie e publique seus formulários
+              </p>
+            </div>
+            <FormsPrimaryButtons />
+          </div>
+          <FormsTable search={search} navigate={navigate} />
+        </Main>
+      </FormsProvider>
     </>
   );
 }
