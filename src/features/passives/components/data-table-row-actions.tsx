d297594bcb66be92661e4034f69d0399e5f43773
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-11 12:42:39 -0300

    feat: create data table actions component (Duplicated: need refactor)

---
+++ src/features/passives/components/data-table-row-actions.tsx
@@ -0,0 +1,64 @@
+import { DotsHorizontalIcon } from '@radix-ui/react-icons'
+import { type Row } from '@tanstack/react-table'
+import { Trash2, UserPen } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+type DataTableRowActionsProps<T> = {
+  row: Row<T>
+}
+
+export function DataTableRowActions<T>({ row }: DataTableRowActionsProps<T>) {
+//   const { setOpen, setCurrentRow } = useUsers()
+  return (
+    <div className='z-100'>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant='ghost'
+            className='data-[state=open]:bg-muted flex h-8 w-8 p-0'
+            onClick={(e) => e.stopPropagation()}
+            onMouseDown={(e) => e.stopPropagation()}
+          >
+            <DotsHorizontalIcon className='h-4 w-4' />
+            <span className='sr-only'>Open menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end' className='w-[160px]'>
+          <DropdownMenuItem
+            onClick={(e) => {
+              e.stopPropagation();
+            //   setCurrentRow(row.original)
+            //   setOpen('view')
+            }}
+            onMouseDown={(e) => e.stopPropagation()}
+          >
+            View
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={(e) => {
+              e.stopPropagation()
+            //   setCurrentRow(row.original)
+            //   setOpen('delete')
+            }}
+            onMouseDown={(e) => e.stopPropagation()}
+            className='text-red-500!'
+          >
+            Delete
+            <DropdownMenuShortcut>
+              <Trash2 size={16} />
+            </DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
