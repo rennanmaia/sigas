73a9af64c2fecac79bbc35ef29ddbe329a65e6c2
kelw.prod@gmail.com <kelw.prod@gmail.com>
2026-01-18 15:48:05 -0300

    feat: improve i18n ressource type

--- src/i18n/types.ts
+++ src/i18n/types.ts
@@ -1,7 +1,125 @@
 export type I18nResource = {
   common: {
     buttons: {
-      DOWNLOAD: string;
-    };
+      download: string;
+      globalSearch: string;
+    },
+    pagination: {
+      pageOf: string;
+      rowsPer: string;
+    },
+    table: {
+      viewOptions: {
+        title: string;
+        menuTitle: string;
+      };
+    }
   };
+  dashboard: {
+    title: string;
+    tabs: {
+      overview: string;
+      analytics: string;
+      reports: string;
+      notifications: string;
+    }
+  },
+  users: {
+    list: {
+      title: string;
+      description: string;
+      buttons: {
+        add: string;
+      },
+      table: {
+        searchPlaceholder: string;
+        noResults: string;
+        filters: {
+          name: string;
+          status: string;
+          roles: string;
+        },
+        headers: {
+          username: string;
+          fullName: string;
+          cpf: string;
+          email: string;
+          status: string;
+          roles: string;
+        },
+        columns: {
+          actions: {
+            view: string;
+            delete: string;
+          }
+        },
+      }
+    },
+    create: {
+      title: string;
+      description: string;
+      submit: {
+        message: string;
+      },
+      form: {
+        cpf: {
+          label: string;
+          placeholder: string;
+          validation: {
+            required: string;
+            invalid: string;
+          }
+        },
+        fullname: {
+          label: string;
+          placeholder: string;
+          validation: {
+            required: string;
+          }
+        },
+        email: {
+          label: string;
+          placeholder: string;
+          validation: {
+            invalid: string;
+          }
+        },
+        roles: {
+          label: string;
+          selectionPlaceholder: string;
+          placeholder: string;
+          notFound: string;
+          validation: {
+            required: string;
+          }
+        },
+        description: {
+          label: string;
+          placeholder: string;
+        },
+      }
+      actions: {
+        create: string;
+        cancel: string;
+      }
+    }
+  }
 };
+
+
+type Join<K, P> = K extends string
+  ? P extends string
+    ? `${K}.${P}`
+    : never
+  : never;
+
+type Leaves<T> = T extends object
+  ? {
+      [K in keyof T]: T[K] extends object
+        ? Join<K & string, Leaves<T[K]>>
+        : K & string;
+    }[keyof T]
+  : never;
+
+type I18nKey = Leaves<I18nResource>;
+export const i18nRessourceMessage = <T extends I18nKey>(key: T) => key;
