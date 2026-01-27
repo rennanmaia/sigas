cc241c7d90773586952ed8e3b1b3b43149dddcdd
João Carvalho <cjoao7610@gmail.com>
2026-01-27 13:41:03 -0300

    feat: add validation schema for projectId in CreateForm route

--- src/routes/_authenticated/forms/create/index.tsx
+++ src/routes/_authenticated/forms/create/index.tsx
@@ -1,6 +1,12 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute } from "@tanstack/react-router";
 import CreateForm from "@/features/forms/create";
+import { z } from "zod";
+
+const createFormSearchSchema = z.object({
+  projectId: z.string().optional(),
+});
 
 export const Route = createFileRoute("/_authenticated/forms/create/")({
   component: CreateForm,
+  validateSearch: createFormSearchSchema,
 });
