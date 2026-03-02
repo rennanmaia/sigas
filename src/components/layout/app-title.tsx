c5bb9cea54efdc4560bbcecf4d4d8773323ca106
João Carvalho <cjoao7610@gmail.com>
2026-03-02 15:12:18 -0300

    refactor(AppTitle): clean up code formatting and improve structure

--- src/components/layout/app-title.tsx
+++ src/components/layout/app-title.tsx
@@ -1,64 +1,42 @@
-import { Link } from '@tanstack/react-router'
-import { Menu, X } from 'lucide-react'
-import { cn } from '@/lib/utils'
+import { Link } from "@tanstack/react-router";
+
+import logoSigas from "@/assets/sigas-logo-icon.png";
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
-} from '@/components/ui/sidebar'
-import { Button } from '../ui/button'
+} from "@/components/ui/sidebar";
 
 export function AppTitle() {
-  const { setOpenMobile } = useSidebar()
+  const { setOpenMobile } = useSidebar();
   return (
     <SidebarMenu>
       <SidebarMenuItem>
         <SidebarMenuButton
-          size='lg'
-          className='gap-0 py-0 hover:bg-transparent active:bg-transparent'
+          size="lg"
+          className="gap-0 py-0 hover:bg-transparent active:bg-transparent"
           asChild
         >
           <div>
             <Link
-              to='/'
+              to="/"
               onClick={() => setOpenMobile(false)}
-              className='grid flex-1 text-start text-sm leading-tight'
+              className="grid flex-1 text-start text-sm leading-tight"
             >
-              <span className='truncate font-bold'>SIGAS</span>
-              <span className='truncate text-xs'>Sistema de Gestão Ambiental Social</span>
+              <div className="flex items-center gap-2">
+                <img src={logoSigas} alt="" className="h-8 w-8 shrink-0" />
+                <span
+                  className="truncate font-bold text-sm"
+                  title="Sistema Integrado de Gestão Social e Ambiental"
+                >
+                  SIGAS
+                </span>
+              </div>
             </Link>
-            <ToggleSidebar />
           </div>
         </SidebarMenuButton>
       </SidebarMenuItem>
     </SidebarMenu>
-  )
-}
-
-function ToggleSidebar({
-  className,
-  onClick,
-  ...props
-}: React.ComponentProps<typeof Button>) {
-  const { toggleSidebar } = useSidebar()
-
-  return (
-    <Button
-      data-sidebar='trigger'
-      data-slot='sidebar-trigger'
-      variant='ghost'
-      size='icon'
-      className={cn('aspect-square size-8 max-md:scale-125', className)}
-      onClick={(event) => {
-        onClick?.(event)
-        toggleSidebar()
-      }}
-      {...props}
-    >
-      <X className='md:hidden' />
-      <Menu className='max-md:hidden' />
-      <span className='sr-only'>Toggle Sidebar</span>
-    </Button>
-  )
+  );
 }
